ff373e802a45bc333df99e0b5d3a0f6aa40775b6
sameheg <webyhost@gmail.com>
2025-09-08 22:35:06 +0300

    Add order events and status listeners

---
+++ app/Events/OrderShipped.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderShipped implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Order $order) {}
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel('orders.'.$this->order->tenant_id);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,19 @@
 
 namespace App\Providers;
 
+use App\Events\OrderCreated;
+use App\Events\OrderShipped;
 use App\Listeners\AddEmployeeFromCv;
 use App\Listeners\InitializeTenant;
 use App\Listeners\LogTicketFeedbackToCrm;
+use App\Listeners\SetOrderStatusCreated;
+use App\Listeners\SetOrderStatusShipped;
 use App\Support\EventBus;
 use App\Support\ModuleRegistry;
 use App\Support\TenantManager;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,6 +32,9 @@ class AppServiceProvider extends ServiceProvider
         $bus->subscribe('tenant.created', InitializeTenant::class);
         $bus->subscribe('ticket.resolved', LogTicketFeedbackToCrm::class);
         $bus->subscribe('candidate.accepted', AddEmployeeFromCv::class);
+
+        Event::listen(OrderCreated::class, [SetOrderStatusCreated::class, 'handle']);
+        Event::listen(OrderShipped::class, [SetOrderStatusShipped::class, 'handle']);
 
         RateLimiter::for('global', function (Request $request) {
             return Limit::perMinute(config('security.throttle_per_minute'))

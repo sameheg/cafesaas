3ce9165245dea627bccc9b0eb136bc0353d22454
sameheg <webyhost@gmail.com>
2025-09-09 01:29:57 +0300

    fix: import inventory and pricing models

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\Inventory\Models\RecipeItem;
 
 class Recipe extends Model
 {
--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\DB;
+use Modules\Pricing\Services\PricingService;
 
 class CartService
 {
